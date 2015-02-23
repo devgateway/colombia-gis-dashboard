f78b7c3104b0f063a7cba7e051417c1be228b419
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-02-23 14:53:00 -0300

    Main Layout

--- client/scripts/components/root.jsx
+++ client/scripts/components/root.jsx
@@ -2,21 +2,21 @@
 
 var React = require('react');
 var RouteHandler = require('react-router').RouteHandler;
-
+var Filter=require('./filter/filter.jsx')
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className="usaid-colombia-gis">
-        <div className="row">
-           <div className="col-lg-3">
-             Filters !
+        <div className="row layout_root">
+           <div className="col-md-4 col-lg-3 hidden-sm hidden-xs layout_filter">
+            <Filter/>
            </div>
+          <div className="col-md-8 col-lg-9 col-sm-12  layout_map">
             {/* defer to the child route handler */}
             <RouteHandler {...this.props} /* <- sends props as attributes to child handler */ />
-            
-        <div>
-      </div>
+            </div>
+        </div>
+      
     );
   }
 });
