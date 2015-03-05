e2a2c112d838db3719b37ccbccf5d9de7fd22b33
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-03-05 09:46:24 -0300

    Base Map Fix

--- client/scripts/components/baseMaps.jsx
+++ client/scripts/components/baseMaps.jsx
@@ -1,17 +1,26 @@
 'use strict';
 
 var React = require('react/addons')
-var Reflux = require('reflux');
 var RouteHandler = require('react-router').RouteHandler;
 var SlideBar=require('./containers/slideBar.jsx');
 var MapActions=require('../actions/mapActions.js')
-var MapActions=require('../actions/mapActions.js');
-var MapStore=require('../stores/mapStore.js');
 var Link = require('react-router').Link;
 
 
+/*
+    <span> value="Topographic">Topographic<options>
+    <option value="Streets">Streets</option>
+    <option value="NationalGeographic">National Geographic<options>
+    <option value="Oceans">Oceans<options>
+    <option value="Gray">Gray<options>
+    <option value="DarkGray">Dark Gray<options>
+    <option value="Imagery">Imagery<options>
+    <option value="ShadedRelief">Shaded Relief<options>
+
+ */
+
 var BaseMapItem= React.createClass({
-  
+
    handleClick: function(event) {
      MapActions.changeBaseMap(this.state.value);
   },
@@ -21,9 +30,25 @@ var BaseMapItem= React.createClass({
   },
 
   render: function() {
-    var currentBaseMap = MapStore.getCurrentBaseMap();
-    return(<span  className="basemap-option" onClick={this.handleClick}> {this.state.label} </span>);
-        
+    return(<span onClick={this.handleClick} className="btn">{this.state.label}</span>);
   }
 
+});
+
+
+module.exports = React.createClass({
+  
+  render: function() {
+
+   return(   
+      <div className="small">BASE MAP 
+      <BaseMapItem label="Gray" value="Gray"/> 
+      <BaseMapItem label="Topographic" value="Topographic"/> 
+      <BaseMapItem label="National Geographic" value="NationalGeographic"/> 
+      <BaseMapItem label="Dark Gray" value="DarkGray"/> 
+      <BaseMapItem label="Imagery" value="Imagery"/> 
+      <BaseMapItem label="Streets" value="Streets"/>
+      </div>   
+    );
+  }
 });
