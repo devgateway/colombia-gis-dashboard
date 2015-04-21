5000ca9a2ee9a7a8d45a4dfc75991605c46c291a
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-04-21 12:22:40 -0300

    Enabling nodejs clustering

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -5,7 +5,7 @@ window.ESRI_AUTH2_URL = 'https://www.arcgis.com/sharing/oauth2/authorize?client_
 window.ESRI_SEARCH_URL = 'http://www.arcgis.com/sharing/rest/search?f=json&';
 window.ESRI_SELF_URL='https://www.arcgis.com/sharing/rest/portals/self';
 
-window.ESRI_PROXY_URL = 'http://localhost:3553'
+window.ESRI_PROXY_URL = 'http://esri-proxy.adstg.org/'
 window.ESIR_USE_PROXY = true;
 //var Message=require('./components/commons/message.jsx');	
 window.Message=require('./components/commons/message.jsx');
