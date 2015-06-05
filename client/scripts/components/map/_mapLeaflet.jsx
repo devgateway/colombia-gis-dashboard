26d831849c30dadbc49c1723bcbd79b45213c448
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-06-05 16:42:32 -0300

    Fixing basemap typo

--- client/scripts/components/map/_mapLeaflet.jsx
+++ client/scripts/components/map/_mapLeaflet.jsx
@@ -55,7 +55,7 @@ module.exports = React.createClass({
   },
 
   componentWillReceiveProps: function(nextProps) {
-    if (nextProps.baseMap && nextProps.baseMap!=this.props.basemap) {
+    if (nextProps.baseMap && nextProps.baseMap!=this.props.baseMap) {
       console.log('map->_mapLeaflet>componentWillReceiveProps Change Map ' + nextProps.baseMap);
       this.setBaseMap(nextProps.baseMap);
     }
