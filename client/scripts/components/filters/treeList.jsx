28eaf3f0433035dc09bdd05c3e8001c862c666b7
mbertoli <mfbertoli@developmentgateway.org>
2015-06-18 11:44:27 -0300

    counter and selections

--- client/scripts/components/filters/treeList.jsx
+++ client/scripts/components/filters/treeList.jsx
@@ -9,10 +9,17 @@ var If=require('../commons/if.jsx');
 var TreeView =  React.createClass({
 
 
-  |onStatusChange: function(status) {
-     this.setState(_.clone(status)); //make a copy of the state would make sense 
-   },
+  onStatusChange: function(status) {
+    this.setState(_.clone(status)); //make a copy of the state would make sense 
+    if (this.props.onItemsLoaded){
+      this.props.onItemsLoaded(this._getItemsFiltered());
+    }
+  },
 
+  _getItemsFiltered: function() {
+    return (this.props.parentIdValue)? this._filterItems(this.state.items,this.props.parentField,this.props.parentIdValue):this.state.items;
+  },
+  
   _addSelected: function(list, id) {
     options = options || {};
     list.push(id);
@@ -39,8 +46,45 @@ var TreeView =  React.createClass({
     this.setState(_.assign(this.state, {
       'selected': newSelection
     }));
+
+    if (this.props.onSelectionChange){
+      this.props.onSelectionChange(this.state.selected);
+    }
   },
 
+  _filterItems: function(items,field,value){
+    return _.filter(items,
+      function(item){
+        return item[field]==value;
+      })
+  },
+
+  _toggle:function(){
+    this.setState(_.assign(this.state,{expanded:!this.state.expanded}));
+  },
+
+  _onSelectAll: function() {
+    var selection = [];
+    this._getItemsFiltered().map(function(item) {
+      this._addSelected(selection, item.id)
+    }, this);
+    this._triggerSelectionChange(selection);
+  },
+
+  _onSelectNone: function() {
+    this._triggerSelectionChange([]);
+  },
+
+  componentWillReceiveProps: function(nextProps){
+    if (nextProps.selectAll!=undefined && nextProps.selectAll!=this.props.selectAll){
+      if (nextProps.selectAll){
+        this._onSelectAll();
+      } else {
+        this._onSelectNone();
+      }
+    }
+  },
+  
   componentDidMount: function() {
     if (this.props.store){
       this.unsubscribe = this.props.store.listen(this.onStatusChange);
@@ -63,36 +107,26 @@ var TreeView =  React.createClass({
     };
   },
 
-  _filterItems:function(items,field,value){
-    return _.filter(items,
-      function(item){
-        return item[field]==value;
-      })
-  },
-  
-  _toggle:function(){
-    debugger;
-    this.setState(_.assign(this.state,{expanded:!this.state.expanded}));
-  },
-
   render: function() {
-    var items = (this.props.parentIdValue)? this._filterItems(this.state.items,this.props.parentField,this.props.parentIdValue):this.state.items;
-    console.log(this.props.label +' - '+ this.state.expanded)
-    
-    return ( 
-    <ul>{this.props.label}
-    {
-      items.map(function(item) {        
-        return (
-          <ul>
-            <Item {...item} showToggler={this.props.nested!=undefined}> 
-             {(this.props.nested)? <TreeView {...this.props.nested} parentIdValue={item.id} />:null}
-            </Item>
-          </ul>)            
-      }.bind(this))
+    var items = this._getItemsFiltered();
+    if (!this.props.collapsed){
+      return ( 
+      <ul>{this.props.label}
+      {
+        items.map(function(item) {    
+          return (
+            <ul>
+              <Item {...item} onItemChange={this._onItemChange} showToggler={this.props.nested!=undefined} selected={this.state.selected.indexOf(item.id)> -1}> 
+                {(this.props.nested)? <TreeView {...this.props.nested} parentIdValue={item.id}/>:null}
+              </Item>
+            </ul>)            
+        }.bind(this))
+      }
+      </ul>
+      );
+    } else {
+      return null;
     }
-    </ul>
-    );
  }
 });
 
@@ -103,41 +137,51 @@ module.exports = React.createClass({
   mixins: [PureRenderMixin],
   
   _onItemChange: function(id, selected) {
-   console.log('TreeList -> _onItemChange');
+    console.log('TreeList -> _onItemChange');
+  },
 
- },
-
- _onSearch: function(keyword) {
-   console.log('TreeList -> _onSearch');
-
- },
+  _onSearch: function(keyword) {
+    console.log('TreeList -> _onSearch');
+  },
 
 
- _onSearchEnterKey: function() {
-   console.log('TreeList -> _onSearchEnterKey');
+  _onSearchEnterKey: function() {
+    console.log('TreeList -> _onSearchEnterKey');
+  },
 
- },
+  _onSelectAll: function() {
+    this.setState(_.assign(this.state,{selectAll: true}));
+    this.forceUpdate();
+  },
 
- _onSelectAll: function() {
-   console.log('TreeList -> _onSelectAll');
- },
+  _onSelectNone: function() {
+    this.setState(_.assign(this.state,{selectAll: false}));
+    this.forceUpdate();
+  },
 
+  getInitialState: function() {
+    return {
+      selectAll: false
+    };
+  },
 
- render: function() {
+  render: function() {
+    console.log('TreeList -> Render');
+    var children = React.Children.map(this.props.children, function(child) {
+      return child ? 
+      React.addons.cloneWithProps(child, { 
+        onSearch:this._onSearch, onSearchEnterKey:this._onSearchEnterKey, 
+        onSelectAll:this._onSelectAll, onSelectNone:this._onSelectNone}) 
+      : null;
+    }, this);
 
-  console.log('TreeList -> Render');
-  var children = React.Children.map(this.props.children, function(child) {
-
-    return child ? React.addons.cloneWithProps(child, { 
-      onSearch:this._onSearch, onSearchEnterKey:this._onSearchEnterKey, onSelectAll:this._onSelectAll, onSelectNone:this._onSelectNone}) : null;}, this);
-
-  return ( 
-   <div> 
-   {children}
-   <span className="filter-label" role="label">{<Message message={this.props.label}/>}</span>
-   <TreeView   {...this.props.nested}/>
-   </div>
-   );
-}
+    return ( 
+      <div> 
+        {children}
+        <span className="filter-label" role="label">{<Message message={this.props.label}/>}</span>
+        <TreeView {...this.props.nested} selectAll={this.state.selectAll}/>
+      </div>
+      );
+  }
 
 });
--- client/scripts/components/filters/treeListItem.jsx
+++ client/scripts/components/filters/treeListItem.jsx
@@ -11,13 +11,26 @@ module.exports = React.createClass({
   
   _onItemChange:function(selected){
     if (this.props.onItemChange){
-      this.props.onItemChange(this.props.id,selected); //21,true|false
+      this.props.onItemChange(this.props.id,selected); 
     }
     this.setState({'selected':selected})
   },
 
   _handleClick:function(){
     this._onItemChange(!this.state.selected);
+  },
+
+  _updateSelectionCount:function(items){
+    this.setState({'childrenSelected': items.length});
+  },
+
+  _updateTotalCount:function(items){
+    this.setState({'childrenTotal': items.length});
+  },
+  
+  _toggle:function(){
+    this.setState(_.assign(this.state,{expanded:!this.state.expanded}))
+    this.forceUpdate();
   },
 
   componentWillReceiveProps :function(nextProps){
@@ -27,26 +40,36 @@ module.exports = React.createClass({
   },
 
   getInitialState: function() {
-    return {selected: this.props.selected,expanded:false};
-  },
-
-  _toggle:function(){
-
-    this.setState(_.assign(this.state,{expanded:!this.state.expanded}))
-    this.forceUpdate();
+    return {
+      selected: this.props.selected, 
+      expanded:false, 
+      childrenSelected:0
+    };
   },
 
   render: function() {
     var itemClassNames=(this.state.selected===true)?'item-label label-selected':'item-label';
+    var children = React.Children.map(this.props.children, function(child) {
+      return child ? 
+      React.addons.cloneWithProps(child, { 
+        onSelectionChange: this._updateSelectionCount,
+        onItemsLoaded: this._updateTotalCount,
+        selectAll: this.state.selected,
+        collapsed: !this.state.expanded
+      }) 
+      : null;
+    }, this);
+    debugger;    
     return(  
       <li>
-        <div onClick={this._handleClick} className="filter-col">
-        <CustomCheckbox selected={this.state.selected} value={this.props.id} onChange={this._onItemChange}/>
-        <span className={itemClassNames}> {this.props.name}</span>
+        <div className="filter-col">
+          <CustomCheckbox selected={this.state.selected} value={this.props.id} onChange={this._handleClick}/>
+          <span onClick={this._handleClick} className={itemClassNames}> {this.props.name}</span>
         </div>
-        <If condition={this.props.showToggler}>
+        <If condition={children}>
+          <div> ({this.state.childrenSelected} / {this.state.childrenTotal}) </div>
           <div onClick={this._toggle}>{(this.state.expanded)?<span>-</span>:<span>+</span>}</div>
-          {(this.state.expanded)?this.props.children:null}  
+          {children}  
         </If>
       </li>
     )
