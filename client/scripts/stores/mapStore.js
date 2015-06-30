8298c292c47c30508c48b7520823e2f5e989b335
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2015-06-30 12:16:17 -0300

    Fix for saving the zoom

--- client/scripts/stores/mapStore.js
+++ client/scripts/stores/mapStore.js
@@ -5,7 +5,9 @@ var Reflux = require('reflux');
 var MapActions = require('../actions/mapActions.js');
 var LoadingActions = require('../actions/loadingActions.js');
 
-var CommonsMixins = require('./_mixins.js')
+var CommonsMixins = require('./_mixins.js');
+
+var defaultZoom = 6;
 
 module.exports = Reflux.createStore({
 
@@ -25,12 +27,14 @@ module.exports = Reflux.createStore({
     this.update({ loading: false });
   },
 
-  onChangeBounds: function(newBounds) {
-    this.update({ bounds: newBounds });
+  onChangeBounds: function(newBounds, newZoom) {
+    var varZoom = newZoom?newZoom:defaultZoom;
+    this.update({ bounds: newBounds, zoom: varZoom});
   },
 
-  onChangeBoundsUser: function(newBounds) {
-    this.update({ bounds: newBounds }, { silent: true });
+  onChangeBoundsUser: function(newBounds, newZoom) {
+    var varZoom = newZoom?newZoom:defaultZoom;
+    this.update({ bounds: newBounds, zoom: varZoom}, { silent: true });
   },
 
   onChangeBaseMap:function(newBaseMap){
@@ -42,8 +46,9 @@ module.exports = Reflux.createStore({
       baseMap : 'Gray',
       bounds: [ [7, -62],
                 [0, -83] ],
+      zoom:defaultZoom,
       loading: false,
-      saveItems:['baseMap', 'bounds']          
+      saveItems:['baseMap', 'bounds', 'zoom']          
     });
   }
 
