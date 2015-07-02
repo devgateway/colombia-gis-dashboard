e78eeff190dbc84254a2d14b9fc516ccaaac4f13
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2015-07-02 11:21:36 -0300

    local infowindow

--- client/scripts/api/infoWindow.js
+++ client/scripts/api/infoWindow.js
@@ -11,11 +11,11 @@ module.exports = {
     getInfoFromAPI: function(infoWindowFilter, filters) {
       console.log("scripts->api->infoWindow: getInfoFromAPI");
       return reqwest({
-        url: 'http://test.monitor.net.co/GisService.svc/Filters/Clusters/Json', 
-        //url: 'mock-data/infoWindowData.json',
+        //url: 'http://test.monitor.net.co/GisService.svc/Filters/Clusters/Json', 
+        url: 'mock-data/infoWindowData.json',
         type: 'json', 
-        method: 'post', 
-        //method: 'get', 
+        //method: 'post', 
+        method: 'get', 
         contentType: "application/json",
         data: JSON.stringify({"filtersInfoWin": infoWindowFilter? infoWindowFilter : [], "filters": filters? filters : []}),
         crossOrigin: true
