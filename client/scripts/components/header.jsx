d761a839c274b7f69db7a3637c85fd0b0debb2af
Sebastian Dimunzio <sdimunzio@gmail.com>
2015-07-30 10:35:42 -0300

    Save Map Dialog Using react boostrap components

--- client/scripts/components/header.jsx
+++ client/scripts/components/header.jsx
@@ -6,7 +6,7 @@ var LanSelector=require('./lanSelector.jsx');
 var SaveStore=require('../stores/saveStore.js');
 var SaveActions=require('../actions/saveActions.js');
 var SaveStore=require('../stores/saveStore.js');
-
+var SaveMap=require('./util/saveMapDialog.jsx')
 var AGOLProfile=require('./esri/AGOLProfile.jsx');
 module.exports = React.createClass({
 
@@ -37,43 +37,14 @@ module.exports = React.createClass({
                     <div className="header-nav">
                       <AGOLProfile/>
                       <a href="#">Menu</a>&nbsp;&nbsp;
-                      <a href="#" data-toggle="modal" data-target="#myModal" onClick={this.handleClickForSave.bind(this)}>Save</a>&nbsp;&nbsp;
-                      <a href="#" onClick={this.handleClickForRestore.bind(this)}>Restore</a>
+                      <SaveMap/>
                       <LanSelector/>
                     </div>
 
                   </div>
                 </div>
               </div>
-              <div className="panel panel-default dialog-save-map" id="saveMapPopup">
-                <div className="panel-heading">
-                  <h3><i className="fa fa-folder-open"></i><Message message='savemap.savemaplabel'/></h3>
-                  <a class="leaflet-popup-close-button" href="#close"><i className="fa fa-times-circle-o"></i></a>
-                </div>
-
-                <div className="panel-body panel-body-savemap">
-
-                    <input className="form-control" type="text" placeholder={i18n.t('savemap.savemaptitle')} ref=""/>
-                    <textarea className="form-control" rows="3" placeholder={i18n.t('savemap.savemapdescription')}></textarea>
               
-                </div>
-
-                <div className="panel-body-savemap plain">
-                  <h3><Message message='savemap.savemaptags'/></h3>
-                  <input className="form-control taginput" type="text" ref=""/>
-
-                  <div className="input-group">
-                    <h4><Message message='savemap.savemapvisibility'/></h4>
-
-                    <div className="layer-search-options">
-                    <ul>
-                      <li><span className="selectable selected"></span><Message message='savemap.savemapprivate'/></li>
-                      <li><span className="selectable"></span><Message message='savemap.savemappublic'/></li>
-                    </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
     );
   }
