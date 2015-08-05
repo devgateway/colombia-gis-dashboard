7fd7160b1f64caacef17c017c7874799da8d847d
mbertoli <mfbertoli@developmentgateway.org>
2015-08-05 18:03:53 -0300

    SRICO-471 _

--- client/scripts/components/header.jsx
+++ client/scripts/components/header.jsx
@@ -7,6 +7,7 @@ var SaveStore=require('../stores/saveStore.js');
 var SaveActions=require('../actions/saveActions.js');
 var SaveStore=require('../stores/saveStore.js');
 var SaveMap=require('./storedMaps/saveDialog.jsx')
+var NationalSubActivities=require('./map/nationalSubActivities.jsx')
 var AGOLProfile=require('./esri/AGOLProfile.jsx');
 module.exports = React.createClass({
 
@@ -37,6 +38,7 @@ module.exports = React.createClass({
                     <div className="header-nav">
                       <AGOLProfile/>
                       <a href="#">Menu</a>&nbsp;&nbsp;
+                      <NationalSubActivities/>&nbsp;&nbsp;
                       <SaveMap/>
                       <LanSelector/>
                     </div>
