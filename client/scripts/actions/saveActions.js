dc98328fcb2a044fc0a18d4c31408c0addc66c51
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2015-08-05 16:28:43 -0300

    SRICO-467 Editing a saved map

--- client/scripts/actions/saveActions.js
+++ client/scripts/actions/saveActions.js
@@ -6,6 +6,7 @@ var Reflux = require('reflux');
 module.exports = { 
   saveMap: Reflux.createAction(),
   openMap: Reflux.createAction(),
+  updateMap: Reflux.createAction(),
   showModal:  Reflux.createAction(),
   hideModal:  Reflux.createAction(),
   findMaps:  Reflux.createAction(),
