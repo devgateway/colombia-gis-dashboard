98eecf3ed2311cf83464cc8dc2f68278c8b0cdbb
mbertoli <mfbertoli@gmail.com>
2015-08-06 11:02:07 -0300

    Deploy August 6th

---
+++ mock-data/exampleMap.js
@@ -0,0 +1,291 @@
+module.exports = {
+	"mapState": {
+		"baseMap": "Gray",
+		"bounds": [
+			[11.350796722383684, -54.3603515625],
+			[-1.252341676699616, -96.5478515625]
+		],
+		"zoom": 6
+	},
+	"lanState": null,
+	"filterData": {},
+	"shapesState": {
+		"breaks": {
+			"field": " - Funding",
+			"breaks": {
+				"Level0": {
+					"min": 0,
+					"max": 20,
+					"style": {
+						"color": {
+							"r": 255,
+							"g": 200,
+							"b": 170,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9
+					}
+				},
+				"Level1": {
+					"min": 20,
+					"max": 40,
+					"style": {
+						"color": {
+							"r": 212,
+							"g": 143,
+							"b": 106,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9
+					}
+				},
+				"Level2": {
+					"min": 40,
+					"max": 60,
+					"style": {
+						"color": {
+							"r": 253,
+							"g": 154,
+							"b": 0,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9
+					}
+				},
+				"Level3": {
+					"min": 60,
+					"max": 80,
+					"style": {
+						"color": {
+							"r": 170,
+							"g": 57,
+							"b": 0,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9
+					}
+				},
+				"Level4": {
+					"min": 80,
+					"max": 100,
+					"style": {
+						"color": {
+							"r": 128,
+							"g": 58,
+							"b": 21,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9
+					}
+				}
+			}
+		},
+		"defaultStyle": {
+			"color": {
+				"r": 255,
+				"g": 255,
+				"b": 255,
+				"a": 0
+			},
+			"weight": 1,
+			"opacity": 1,
+			"fillOpacity": 0.9
+		},
+		"level": "departament",
+		"opacity": 1,
+		"visible": true
+	},
+	"pointsState": {
+		"breaks": {
+			"field": "activities",
+			"breaks": {
+				"Level0": {
+					"min": 0,
+					"max": 20,
+					"style": {
+						"color": {
+							"r": 253,
+							"g": 154,
+							"b": 0,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9,
+						"radius": 20
+					}
+				},
+				"Level1": {
+					"min": 20,
+					"max": 45,
+					"style": {
+						"color": {
+							"r": 253,
+							"g": 154,
+							"b": 0,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9,
+						"radius": 30
+					}
+				},
+				"Level2": {
+					"min": 45,
+					"max": 75,
+					"style": {
+						"color": {
+							"r": 253,
+							"g": 154,
+							"b": 0,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9,
+						"radius": 40
+					}
+				},
+				"Level3": {
+					"min": 75,
+					"max": 175,
+					"style": {
+						"color": {
+							"r": 253,
+							"g": 154,
+							"b": 0,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9,
+						"radius": 50
+					}
+				},
+				"Level4": {
+					"min": 175,
+					"max": 999,
+					"style": {
+						"color": {
+							"r": 253,
+							"g": 154,
+							"b": 0,
+							"a": 0.8
+						},
+						"weight": 1,
+						"opacity": 1,
+						"fillOpacity": 0.9,
+						"radius": 55
+					}
+				}
+			}
+		},
+		"defaultStyle": {
+			"color": {
+				"r": 253,
+				"g": 154,
+				"b": 0,
+				"a": 0.8
+			},
+			"weight": 1,
+			"opacity": 1,
+			"fillOpacity": 0.9
+		},
+		"level": "departament",
+		"opacity": 1,
+		"visible": true
+	},
+	"arcgisState": {
+		"layers": [{
+			"id": "df8aae23082d40988dde319cbd86930c",
+			"layer": {
+				"currentVersion": 10.3,
+				"serviceDescription": "Distritos de riego Colombia",
+				"hasVersionedData": false,
+				"supportsDisconnectedEditing": false,
+				"hasStaticData": false,
+				"maxRecordCount": 1000,
+				"supportedQueryFormats": "JSON",
+				"capabilities": "Create,Delete,Query,Update,Editing",
+				"description": "Distritos de riego Colombia",
+				"copyrightText": "",
+				"spatialReference": {
+					"wkt": "PROJCS[\"GAUSS_BTA_MAGNA\",GEOGCS[\"CGS_SIRGAS\",DATUM[\"CGS_SIRGAS\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",1000000.0],PARAMETER[\"False_Northing\",1000000.0],PARAMETER[\"Central_Meridian\",-74.077507917],PARAMETER[\"Scale_Factor\",1.0],PARAMETER[\"Latitude_Of_Origin\",4.596200417],UNIT[\"Meter\",1.0]]"
+				},
+				"initialExtent": {
+					"xmin": -314328.2110013098,
+					"ymin": 1144295.7209040355,
+					"xmax": 2463802.3452598033,
+					"ymax": 1895713.8904070412,
+					"spatialReference": {
+						"wkt": "PROJCS[\"GAUSS_BTA_MAGNA\",GEOGCS[\"CGS_SIRGAS\",DATUM[\"CGS_SIRGAS\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",1000000.0],PARAMETER[\"False_Northing\",1000000.0],PARAMETER[\"Central_Meridian\",-74.077507917],PARAMETER[\"Scale_Factor\",1.0],PARAMETER[\"Latitude_Of_Origin\",4.596200417],UNIT[\"Meter\",1.0]]"
+					}
+				},
+				"fullExtent": {
+					"xmin": 165326.46263640013,
+					"ymin": 20609.932563383132,
+					"xmax": 1804236.2497373545,
+					"ymax": 1984857.7143642344,
+					"spatialReference": {
+						"wkt": "PROJCS[\"GAUSS_BTA_MAGNA\",GEOGCS[\"CGS_SIRGAS\",DATUM[\"CGS_SIRGAS\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",1000000.0],PARAMETER[\"False_Northing\",1000000.0],PARAMETER[\"Central_Meridian\",-74.077507917],PARAMETER[\"Scale_Factor\",1.0],PARAMETER[\"Latitude_Of_Origin\",4.596200417],UNIT[\"Meter\",1.0]]"
+					}
+				},
+				"allowGeometryUpdates": true,
+				"units": "esriMeters",
+				"size": 1163264,
+				"syncEnabled": false,
+				"editorTrackingInfo": {
+					"enableEditorTracking": false,
+					"enableOwnershipAccessControl": false,
+					"allowOthersToUpdate": true,
+					"allowOthersToDelete": false
+				},
+				"xssPreventionInfo": {
+					"xssPreventionEnabled": true,
+					"xssPreventionRule": "InputOnly",
+					"xssInputRule": "rejectInvalid"
+				},
+				"layers": [{
+					"id": 0,
+					"name": "Distritos de Riego",
+					"parentLayerId": -1,
+					"defaultVisibility": true,
+					"subLayerIds": null,
+					"minScale": 0,
+					"maxScale": 0,
+					"visible": true,
+					"opacity": 1
+				}, {
+					"id": 1,
+					"name": "Departamentos",
+					"parentLayerId": -1,
+					"defaultVisibility": true,
+					"subLayerIds": null,
+					"minScale": 0,
+					"maxScale": 0,
+					"visible": true,
+					"opacity": 1
+				}],
+				"tables": []
+			},
+			"url": "http://services1.arcgis.com/p1fBF7MSQeyrqtIr/arcgis/rest/services/Distritos_de_Riego_Colombia/FeatureServer",
+			"type": "Feature Service",
+			"title": "Distritos_de_Riego_Colombia",
+			"opacity": 1,
+			"visible": true,
+			"created": true,
+			"added": true
+		}]
+	}
+}
