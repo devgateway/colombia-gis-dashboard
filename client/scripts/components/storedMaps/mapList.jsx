2e98d9b721427b0263c0073389c4aba5007fd36b
Gali <gkalvatcheva@developmentgateway.org>
2015-08-13 10:19:36 -0400

    - added png print option (we need  to add back the functionality that triggers the print dialog modal) - adde png print option (need functionality) - translation for print png - styling of saved map area - update

--- client/scripts/components/storedMaps/mapList.jsx
+++ client/scripts/components/storedMaps/mapList.jsx
@@ -103,7 +103,12 @@ mixins: [Reflux.connect(Store,"store")],
                               <i className="pull-right fa fa-pencil" title={i18n.t('savemap.tooltipupdate')} onClick={this._update.bind(this,m._id)}></i>
                               </a>
                               <SaveMap/>
-                              <PrintDialog key={m.id} id={m._id}/>
+                              <a href="#">
+                              <i className="pull-right fa fa-file-pdf-o" title={i18n.t('savemap.tooltipprint')} onClick='{this._update.bind(this,m._id)}'></i>
+                              </a>
+                              <a href="#">
+                             <i className="pull-right fa fa-file-image-o" title={i18n.t('savemap.tooltipprintpng')} onClick=''></i>
+                             </a>
                               <a href="#">
                               <i className="pull-right fa fa-folder-open" title={i18n.t('savemap.tooltipopen')} onClick={this._open.bind(this,m._id)}></i>
                               </a>
