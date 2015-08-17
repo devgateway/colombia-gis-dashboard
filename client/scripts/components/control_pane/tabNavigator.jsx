f0807d22d5482b90f75296649f1e788bad1105f5
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2015-08-17 13:33:15 -0300

    SRICO-566 - Implementations needed for saved maps

--- client/scripts/components/control_pane/tabNavigator.jsx
+++ client/scripts/components/control_pane/tabNavigator.jsx
@@ -8,15 +8,22 @@ var TabPane = require('react-bootstrap/lib/TabPane');
 var Basemaps=require('../map/baseMap.jsx')
 var TabLayerNavigator=require('./../map/tabLayerNavigator.jsx');
 
-var SaveMap=require('./storedMaps/saveDialog.jsx');
+var SaveActions=require('../../actions/saveActions.js');
+var SaveMap=require('../storedMaps/saveDialog.jsx');
 
 var Filter=require('./../filters/filtersManager.jsx');
-var  StoredMaps=require('../storedMaps/mapList.jsx')
-module.exports  = React.createClass({
+var StoredMaps=require('../storedMaps/mapList.jsx');
 
+
+module.exports  = React.createClass({
 
   componentDidMount: function(){
       $(this.getDOMNode()).mCustomScrollbar({theme:"inset-dark"}); //TODO: can't this be done by a css??
+      $( "#saveMapPopup" ).hide();
+  },
+
+  open:function(){
+    SaveActions.showModal('save')
   },
 
   render: function() {
@@ -39,7 +46,7 @@ module.exports  = React.createClass({
               <StoredMaps/>
           </TabPane>
         </TabbedArea>
-
+        <SaveMap/>
       </div>
       );
   }
--- client/scripts/components/header.jsx
+++ client/scripts/components/header.jsx
@@ -3,34 +3,11 @@ var React = require('react');
 var Reflux = require('reflux');
 var RouteHandler = require('react-router').RouteHandler;
 var LanSelector=require('./lanSelector.jsx');
-var SaveStore=require('../stores/saveStore.js');
-var SaveActions=require('../actions/saveActions.js');
-var SaveStore=require('../stores/saveStore.js');
 var SaveMap=require('./storedMaps/saveDialog.jsx');
 var NationalSubActivities=require('./map/nationalSubActivities.jsx')
 var AGOLProfile=require('./esri/AGOLProfile.jsx');
+
 module.exports = React.createClass({
-
-  componentDidMount:function(){
-      $( "#saveMapPopup" ).hide();
-  },
-
-  handleClickForSave:function(){
-    console.log('Save Map');
-    //SaveActions.saveMap();
-    $( "#saveMapPopup" ).dialog();
-
-  },
-
-  handleClickForRestore:function(){
-    console.log('Save Map');
-    SaveActions.restoreMap();
-  },
-
-  open:function(){
-    SaveActions.showModal('save')
-  },
-
   render: function() {
     return (
             <div>
@@ -43,8 +20,6 @@ module.exports = React.createClass({
                       <AGOLProfile/>
                       <a href="#">{i18n.t('app.menu')}</a>&nbsp;&nbsp;
                       <NationalSubActivities/>&nbsp;&nbsp;
-                      <a href="#" data-toggle="modal" data-target="#myModal" onClick={this.open}>Save</a>
-                      <SaveMap/>
                       <LanSelector/>
                     </div>
 
