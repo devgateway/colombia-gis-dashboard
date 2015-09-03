a50ceb5bb418f756e28ad4559e7f9cf8159ecc7e
mbertoli <mfbertoli@developmentgateway.org>
2015-09-03 11:38:29 -0300

    fixing indicator name place

--- client/scripts/components/map/layers/_indicatorsLayerControl.jsx
+++ client/scripts/components/map/layers/_indicatorsLayerControl.jsx
@@ -112,14 +112,10 @@ module.exports = React.createClass({
             <h3 className="color-control"><Message message='layers.classificationScheme'/></h3>
             <div>
               <div className="breaksTemplates">
-                <div className="label label-info" onClick={this._changeBreaksWrapper.bind(this, 0)} title={i18n.t("filters.defaultTip")}><Message message='filters.default'/></div>
-                <div className="label label-info" onClick={this._changeBreaksWrapper.bind(this, 1)} title={i18n.t("filters.jenksTip")}><Message message='filters.jenks'/></div>
-                <div className="label label-info" onClick={this._changeBreaksWrapper.bind(this, 2)} title={i18n.t("filters.arithmeticTip")}><Message message='filters.arithmetic'/></div>
-                <div className="label label-info" onClick={this._changeBreaksWrapper.bind(this, 3)} title={i18n.t("filters.geometricTip")}><Message message='filters.geometric'/></div>
                 <div className={this.state.breakSelected==0?"label label-info-selected":"label label-info"} onClick={this._changeBreaksWrapper.bind(this, 0)} title={i18n.t("filters.defaultTip")}><Message message='filters.default'/></div>
                 <div className={this.state.breakSelected==1?"label label-info-selected":"label label-info"} onClick={this._changeBreaksWrapper.bind(this, 1)} title={i18n.t("filters.jenksTip")}><Message message='filters.jenks'/></div>
                 <div className={this.state.breakSelected==2?"label label-info-selected":"label label-info"} onClick={this._changeBreaksWrapper.bind(this, 2)} title={i18n.t("filters.arithmeticTip")}><Message message='filters.arithmetic'/></div>
-                <div className={this.state.breakSelected==3?"label label-info-selected":"label label-info"} onClick={this._changeBreaksWrapper.bind(this, 3)} title={i18n.t("filters.geometricTip")}><Message message='filters.geometric'/></div>r
+                <div className={this.state.breakSelected==3?"label label-info-selected":"label label-info"} onClick={this._changeBreaksWrapper.bind(this, 3)} title={i18n.t("filters.geometricTip")}><Message message='filters.geometric'/></div>
               </div>
               <div className="clearFix"/>
               <div className="breaksTemplates">
